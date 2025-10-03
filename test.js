f4fe4247f1f807a16ed45ce1f09649772751f68d
Edupe2603 <31553111+Edupe2603@users.noreply.github.com>
2025-10-03 22:52:11 +0000

    Tarea JEST

---
+++ test.js
@@ -0,0 +1,27 @@
+const { sum } = require('./app.js');
+
+test('adds 14 + 9 to equal 23', () => {
+    let total = sum(14, 9);
+    expect(total).toBe(23);
+});
+
+test("One euro should be 1.07 dollars", function () {
+    // Importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js');
+
+    // Uso la función como debe ser usada
+    const dollars = fromEuroToDollar(3.5);
+
+    // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
+    const expected = 3.5 * 1.07;
+
+    // Hago mi comparación (la prueba)
+    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+})
+
+test("10 yenes deben ser 8.7 libras", function () {
+    const { fromYenToPound } = require('./app.js');
+    const libra = fromYenToPound(10)
+    const expected = (10 * 0.87)
+    expect(fromYenToPound(10)).toBe(8.7)
+})
